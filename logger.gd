d3376ff2d538b77a20dc38c1a5bf5f2b55dd089d
hedin <flame.beholder@gmail.com>
2018-10-05 14:07:48 +0300

    fixed default logfile name

--- logger.gd
+++ logger.gd
@@ -238,7 +238,7 @@ const FILE_BUFFER_SIZE = 30
 var default_output_level = INFO
 # TODO: Find (or implement in Godot) a more clever way to achieve that
 var default_output_strategies = [STRATEGY_PRINT, STRATEGY_PRINT, STRATEGY_PRINT, STRATEGY_PRINT, STRATEGY_PRINT]
-var default_logfile_path = "user://%s.log" % ProjectSettings.get_setting("application/name")
+var default_logfile_path = "user://%s.log" % ProjectSettings.get_setting("application/config/name")
 var default_configfile_path = "user://%s.cfg" % PLUGIN_NAME
 
 # e.g. "[INFO] [main] The young alpaca started growing a goatie."
